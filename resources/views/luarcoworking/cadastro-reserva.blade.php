892a06833a00cbe4ff17504fe40132f97be175a3
Anderson Sousa <anderson21br@gmail.com>
2020-02-26 04:07:35 -0300

    corrigindo menu com redes sociais

--- resources/views/luarcoworking/cadastro-reserva.blade.php
+++ resources/views/luarcoworking/cadastro-reserva.blade.php
@@ -188,3 +188,193 @@ var elementorFrontendConfig = {"environmentMode":{"edit":false,"wpPreview":false
 	
 
 </body></html>
+
+<!--  -->
+
+
+<html lang="pt-BR"><head>
+	<meta charset="UTF-8">
+		<title>form-reserva-2 – Luar Coworking</title>
+<link rel="dns-prefetch" href="//s.w.org">
+<link rel="alternate" type="application/rss+xml" title="Feed para Luar Coworking »" href="http://18.220.210.235/luarcoworking/index.php/feed/">
+<link rel="alternate" type="application/rss+xml" title="Feed de comentários para Luar Coworking »" href="http://18.220.210.235/luarcoworking/index.php/comments/feed/">
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/18.220.210.235\/luarcoworking\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.3.2"}};
+			!function(e,a,t){var r,n,o,i,p=a.createElement("canvas"),s=p.getContext&&p.getContext("2d");function c(e,t){var a=String.fromCharCode;s.clearRect(0,0,p.width,p.height),s.fillText(a.apply(this,e),0,0);var r=p.toDataURL();return s.clearRect(0,0,p.width,p.height),s.fillText(a.apply(this,t),0,0),r===p.toDataURL()}function l(e){if(!s||!s.fillText)return!1;switch(s.textBaseline="top",s.font="600 32px Arial",e){case"flag":return!c([127987,65039,8205,9895,65039],[127987,65039,8203,9895,65039])&&(!c([55356,56826,55356,56819],[55356,56826,8203,55356,56819])&&!c([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]));case"emoji":return!c([55357,56424,55356,57342,8205,55358,56605,8205,55357,56424,55356,57340],[55357,56424,55356,57342,8203,55358,56605,8203,55357,56424,55356,57340])}return!1}function d(e){var t=a.createElement("script");t.src=e,t.defer=t.type="text/javascript",a.getElementsByTagName("head")[0].appendChild(t)}for(i=Array("flag","emoji"),t.supports={everything:!0,everythingExceptFlag:!0},o=0;o<i.length;o++)t.supports[i[o]]=l(i[o]),t.supports.everything=t.supports.everything&&t.supports[i[o]],"flag"!==i[o]&&(t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&t.supports[i[o]]);t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&!t.supports.flag,t.DOMReady=!1,t.readyCallback=function(){t.DOMReady=!0},t.supports.everything||(n=function(){t.readyCallback()},a.addEventListener?(a.addEventListener("DOMContentLoaded",n,!1),e.addEventListener("load",n,!1)):(e.attachEvent("onload",n),a.attachEvent("onreadystatechange",function(){"complete"===a.readyState&&t.readyCallback()})),(r=t.source||{}).concatemoji?d(r.concatemoji):r.wpemoji&&r.twemoji&&(d(r.twemoji),d(r.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script><script src="http://18.220.210.235/luarcoworking/wp-includes/js/wp-emoji-release.min.js?ver=5.3.2" type="text/javascript" defer=""></script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+	<link rel="stylesheet" id="wp-block-library-css" href="http://18.220.210.235/luarcoworking/wp-includes/css/dist/block-library/style.min.css?ver=5.3.2" type="text/css" media="all">
+<link rel="stylesheet" id="hello-elementor-css" href="http://18.220.210.235/luarcoworking/wp-content/themes/hello-elementor/style.min.css?ver=2.2.0" type="text/css" media="all">
+<link rel="stylesheet" id="hello-elementor-theme-style-css" href="http://18.220.210.235/luarcoworking/wp-content/themes/hello-elementor/theme.min.css?ver=2.2.0" type="text/css" media="all">
+<link rel="stylesheet" id="elementor-icons-css" href="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.6.2" type="text/css" media="all">
+<link rel="stylesheet" id="elementor-animations-css" href="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/lib/animations/animations.min.css?ver=2.9.2" type="text/css" media="all">
+<link rel="stylesheet" id="elementor-frontend-css" href="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/css/frontend.min.css?ver=2.9.2" type="text/css" media="all">
+<link rel="stylesheet" id="elementor-global-css" href="http://18.220.210.235/luarcoworking/wp-content/uploads/elementor/css/global.css?ver=1582588229" type="text/css" media="all">
+<link rel="stylesheet" id="elementor-post-144-css" href="http://18.220.210.235/luarcoworking/wp-content/uploads/elementor/css/post-144.css?ver=1582700762" type="text/css" media="all">
+<link rel="stylesheet" id="google-fonts-1-css" href="https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;ver=5.3.2" type="text/css" media="all">
+<link rel="stylesheet" id="elementor-icons-shared-0-css" href="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.12.0" type="text/css" media="all">
+<link rel="stylesheet" id="elementor-icons-fa-brands-css" href="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/lib/font-awesome/css/brands.min.css?ver=5.12.0" type="text/css" media="all">
+<link rel="https://api.w.org/" href="http://18.220.210.235/luarcoworking/index.php/wp-json/">
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://18.220.210.235/luarcoworking/xmlrpc.php?rsd">
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://18.220.210.235/luarcoworking/wp-includes/wlwmanifest.xml"> 
+<meta name="generator" content="WordPress 5.3.2">
+<link rel="canonical" href="http://18.220.210.235/luarcoworking/index.php/form-reserva-2/">
+<link rel="shortlink" href="http://18.220.210.235/luarcoworking/?p=144">
+<link rel="alternate" type="application/json+oembed" href="http://18.220.210.235/luarcoworking/index.php/wp-json/oembed/1.0/embed?url=http%3A%2F%2F18.220.210.235%2Fluarcoworking%2Findex.php%2Fform-reserva-2%2F">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></head>
+<body class="page-template page-template-elementor_canvas page page-id-144 elementor-default elementor-template-canvas elementor-kit-8 elementor-page elementor-page-144" data-elementor-device-mode="mobile" cz-shortcut-listen="true">
+			<div data-elementor-type="wp-page" data-elementor-id="144" class="elementor elementor-144" data-elementor-settings="[]">
+			<div class="elementor-inner">
+				<div class="elementor-section-wrap">
+							<section class="elementor-element elementor-element-7fb86233 elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section" data-id="7fb86233" data-element_type="section">
+						<div class="elementor-container elementor-column-gap-default">
+				<div class="elementor-row">
+				<div class="elementor-element elementor-element-25990c7a elementor-column elementor-col-50 elementor-top-column" data-id="25990c7a" data-element_type="column">
+			<div class="elementor-column-wrap  elementor-element-populated">
+					<div class="elementor-widget-wrap">
+				<div class="elementor-element elementor-element-11eeb796 elementor-widget elementor-widget-image" data-id="11eeb796" data-element_type="widget" data-widget_type="image.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-image">
+										<img width="300" height="85" src="http://18.220.210.235/luarcoworking/wp-content/uploads/2020/02/logo_logotipo_web.png" class="attachment-medium size-medium" alt="">											</div>
+				</div>
+				</div>
+						</div>
+			</div>
+		</div>
+				<div class="elementor-element elementor-element-452b99b2 elementor-hidden-phone elementor-column elementor-col-50 elementor-top-column" data-id="452b99b2" data-element_type="column">
+			<div class="elementor-column-wrap  elementor-element-populated">
+					<div class="elementor-widget-wrap">
+				<div class="elementor-element elementor-element-19751f28 elementor-shape-circle elementor-widget elementor-widget-social-icons" data-id="19751f28" data-element_type="widget" data-widget_type="social-icons.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-social-icons-wrapper">
+							<a class="elementor-icon elementor-social-icon elementor-social-icon-facebook elementor-repeater-item-790e298" target="_blank">
+					<span class="elementor-screen-only">Facebook</span>
+					<i class="fab fa-facebook"></i>				</a>
+							<a class="elementor-icon elementor-social-icon elementor-social-icon-instagram elementor-repeater-item-78935e9" target="_blank">
+					<span class="elementor-screen-only">Instagram</span>
+					<i class="fab fa-instagram"></i>				</a>
+							<a class="elementor-icon elementor-social-icon elementor-social-icon-whatsapp elementor-repeater-item-c476b0f" target="_blank">
+					<span class="elementor-screen-only">Whatsapp</span>
+					<i class="fab fa-whatsapp"></i>				</a>
+					</div>
+				</div>
+				</div>
+						</div>
+			</div>
+		</div>
+						</div>
+			</div>
+		</section>
+				<section class="elementor-element elementor-element-5ea4b9eb elementor-section-height-min-height elementor-section-content-middle elementor-section-boxed elementor-section-height-default elementor-section-items-middle elementor-section elementor-top-section" data-id="5ea4b9eb" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+							<div class="elementor-background-overlay"></div>
+							<div class="elementor-container elementor-column-gap-default">
+				<div class="elementor-row">
+				<div class="elementor-element elementor-element-41cc1ffc elementor-column elementor-col-100 elementor-top-column" data-id="41cc1ffc" data-element_type="column">
+			<div class="elementor-column-wrap  elementor-element-populated">
+					<div class="elementor-widget-wrap">
+				<div class="elementor-element elementor-element-f36846b elementor-widget elementor-widget-heading" data-id="f36846b" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<h1 class="elementor-heading-title elementor-size-xl">Formulário</h1>		</div>
+				</div>
+						</div>
+			</div>
+		</div>
+						</div>
+			</div>
+		</section>
+				<section class="elementor-element elementor-element-18f96993 elementor-section-height-min-height elementor-section-boxed elementor-section-height-default elementor-section-items-middle elementor-section elementor-top-section" data-id="18f96993" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+						<div class="elementor-container elementor-column-gap-default">
+				<div class="elementor-row">
+				<div class="elementor-element elementor-element-6c980a2a elementor-column elementor-col-100 elementor-top-column" data-id="6c980a2a" data-element_type="column">
+			<div class="elementor-column-wrap  elementor-element-populated">
+					<div class="elementor-widget-wrap">
+				<section class="elementor-element elementor-element-78ee05dd elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-inner-section" data-id="78ee05dd" data-element_type="section">
+						<div class="elementor-container elementor-column-gap-default">
+				<div class="elementor-row">
+				<div class="elementor-element elementor-element-6c03508c elementor-column elementor-col-100 elementor-inner-column" data-id="6c03508c" data-element_type="column">
+			<div class="elementor-column-wrap  elementor-element-populated">
+					<div class="elementor-widget-wrap">
+				<div class="elementor-element elementor-element-6eb331c1 elementor-icon-list--layout-traditional elementor-widget elementor-widget-icon-list" data-id="6eb331c1" data-element_type="widget" data-widget_type="icon-list.default">
+				<div class="elementor-widget-container">
+					<ul class="elementor-icon-list-items">
+							<li class="elementor-icon-list-item">
+										<span class="elementor-icon-list-text">Nome</span>
+									</li>
+								<li class="elementor-icon-list-item">
+										<span class="elementor-icon-list-text">E-mail</span>
+									</li>
+								<li class="elementor-icon-list-item">
+										<span class="elementor-icon-list-text">Cel</span>
+									</li>
+						</ul>
+				</div>
+				</div>
+						</div>
+			</div>
+		</div>
+						</div>
+			</div>
+		</section>
+						</div>
+			</div>
+		</div>
+						</div>
+			</div>
+		</section>
+				<section class="elementor-element elementor-element-5dd58053 elementor-section-height-full elementor-section-boxed elementor-section-height-default elementor-section-items-middle elementor-section elementor-top-section" data-id="5dd58053" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+							<div class="elementor-background-overlay"></div>
+							<div class="elementor-container elementor-column-gap-default">
+				<div class="elementor-row">
+				<div class="elementor-element elementor-element-3fe80b3d elementor-column elementor-col-100 elementor-top-column" data-id="3fe80b3d" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+			<div class="elementor-column-wrap  elementor-element-populated">
+							<div class="elementor-background-overlay"></div>
+					<div class="elementor-widget-wrap">
+				<div class="elementor-element elementor-element-32f09ab4 elementor-align-center elementor-widget elementor-widget-button" data-id="32f09ab4" data-element_type="widget" data-widget_type="button.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-button-wrapper">
+			<a href="#" class="elementor-button-link elementor-button elementor-size-lg" role="button">
+						<span class="elementor-button-content-wrapper">
+						<span class="elementor-button-text">Confirmar </span>
+		</span>
+					</a>
+		</div>
+				</div>
+				</div>
+						</div>
+			</div>
+		</div>
+						</div>
+			</div>
+		</section>
+						</div>
+			</div>
+		</div>
+		<script type="text/javascript" src="http://18.220.210.235/luarcoworking/wp-includes/js/wp-embed.min.js?ver=5.3.2"></script>
+<script type="text/javascript" src="http://18.220.210.235/luarcoworking/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp"></script>
+<script type="text/javascript" src="http://18.220.210.235/luarcoworking/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1"></script>
+<script type="text/javascript" src="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/js/frontend-modules.min.js?ver=2.9.2"></script>
+<script type="text/javascript" src="http://18.220.210.235/luarcoworking/wp-includes/js/jquery/ui/position.min.js?ver=1.11.4"></script>
+<script type="text/javascript" src="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/lib/dialog/dialog.min.js?ver=4.7.6"></script>
+<script type="text/javascript" src="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min.js?ver=4.0.2"></script>
+<script type="text/javascript" src="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/lib/swiper/swiper.min.js?ver=5.3.0"></script>
+<script type="text/javascript" src="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/lib/share-link/share-link.min.js?ver=2.9.2"></script>
+<script type="text/javascript">
+var elementorFrontendConfig = {"environmentMode":{"edit":false,"wpPreview":false},"i18n":{"shareOnFacebook":"Share on Facebook","shareOnTwitter":"Share on Twitter","pinIt":"Pin it","downloadImage":"Download image"},"is_rtl":false,"breakpoints":{"xs":0,"sm":480,"md":768,"lg":1025,"xl":1440,"xxl":1600},"version":"2.9.2","urls":{"assets":"http:\/\/18.220.210.235\/luarcoworking\/wp-content\/plugins\/elementor\/assets\/"},"settings":{"page":[],"general":{"elementor_global_image_lightbox":"yes","elementor_lightbox_enable_counter":"yes","elementor_lightbox_enable_fullscreen":"yes","elementor_lightbox_enable_zoom":"yes","elementor_lightbox_enable_share":"yes","elementor_lightbox_title_src":"title","elementor_lightbox_description_src":"description"},"editorPreferences":[]},"post":{"id":144,"title":"form-reserva-2 &#8211; Luar Coworking","excerpt":"","featuredImage":false}};
+</script>
+<script type="text/javascript" src="http://18.220.210.235/luarcoworking/wp-content/plugins/elementor/assets/js/frontend.min.js?ver=2.9.2"></script><span id="elementor-device-mode" class="elementor-screen-only"></span>
+	
+
+</body></html>
